b7bc3d5dcb2582ebd8a4a5341d6060d899687c93
Hunter <52325693+hunterrudnet@users.noreply.github.com>
2023-04-29 20:21:36 -0400

    Add old stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Experience from "./components/Experience";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Container, Box, Typography } from "@mui/material";
+import { Link, animateScroll as scroll } from "react-scroll";
+import ScrollTrigger  from "react-scroll-trigger";
+import About from "./components/About";
+import Education from "./components/Education";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+import "./App.css";
+
+const sections = [
+  { id: "about", label: "About" },
+  { id: "experience", label: "Experience" },
+  { id: "education", label: "Education" },
+  { id: "skills", label: "Skills" },
+  { id: "contact", label: "Contact" },
+];
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Roboto, sans-serif",
+  },
+  components: {
+    MuiButtonBase: {
+      defaultProps: {
+        disableRipple: true,
+      },
+    },
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          borderRadius: "12px",
+        },
+      },
+    },
+    MuiTooltip: {
+      styleOverrides: {
+        tooltip: {
+          borderRadius: "12px",
+        },
+      },
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ThemeProvider theme={theme}>
+    <div>
+      <nav>
+        <ul className="nav-links">
+          {sections.map((section) => (
+            <li key={section.id}>
+              <Link
+                activeClass="active"
+                to={section.id}
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                {section.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <Container maxWidth="md">
+        {sections.map((section) => (
+          <Box key={section.id} id={section.id} sx={{ my: 8 }}>
+            <Typography className="section-title" variant="h4">
+              {section.label}
+            </Typography>
+            <Content id={section.id} />
+          </Box>
+        ))}
+      </Container>
     </div>
+    </ThemeProvider>
   );
+}
+
+function Content({ id }) {
+  const [visible, setVisible] = useState(false);
+
+  const onEnter = () => {
+    setVisible(true);
+  };
+
+  const onExit = () => {
+    setVisible(false);
+  };
+
+  return (
+    <ScrollTrigger onEnter={onEnter} onExit={onExit}>
+      <div className={`section-content${visible ? " visible" : ""}`}>
+        {getContent(id)}
+      </div>
+    </ScrollTrigger>
+  );
+}
+
+function getContent(id) {
+  switch (id) {
+    case "about":
+      return <About/>;
+    case "experience":
+      return <Experience/>
+    case "education":
+      return <Education/>;
+    case "skills":
+      return <Skills/>
+    case "contact":
+      return <Contact/>;
+    default:
+      return null;
+  }
 }
 
 export default App;
